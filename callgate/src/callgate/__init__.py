00875daa7eaca2de2d2e5b41309dd9384ee9f5b5
Dev-Saif-Ops <mohammadsafwanathar27@gmail.com>
2026-08-28 17:06:32 +0530

    Bump version to 0.2.0 for first PyPI release

--- callgate/src/callgate/__init__.py
+++ callgate/src/callgate/__init__.py
@@ -22,7 +22,7 @@ from callgate.shield import Finding, Shield
 from callgate.suggest import suggest_policies
 from callgate.mcp_guard import GuardedCall, MCPGuard, to_mcp_error
 
-__version__ = "0.2.0.dev0"
+__version__ = "0.2.0"
 
 __all__ = [
     "Gate",
